6fc02497ba012f982b85f05846b334bacdd623b0
mohye <mohye20@gmail.com>
2024-11-18 09:15:35 +0200

    Document Setting - Upload File

---
+++ Demo.PresnationLayer/Helpers/DocumentSettings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Demo.PresnationLayer.Helpers
+{
+    public static class DocumentSettings
+    {
+        // Upload
+        public static string UploadFile(IFormFile file, string FolderName)
+        {
+            // 1- Get Located Folder Path
+            string FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+            // 2- Get File Name And Make It Unique
+            string FileName = $"{Guid.NewGuid()}{file.FileName}";
+            // 3- Get File Path[Folder Path + FileName ] 
+            string FilePath = Path.Combine(FolderPath, FileName);
+            // 4- Save File As Streams
+           using var Fs = new FileStream(FilePath , FileMode.Create);
+            file.CopyTo(Fs);
+            // 5- Return File Name 
+            return FileName;
+
+
+        }
+
+        // Delete
+    }
+}
--- Demo.PresnationLayer/obj/Debug/net5.0/Demo.PresnationLayer.AssemblyInfo.cs
+++ Demo.PresnationLayer/obj/Debug/net5.0/Demo.PresnationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.PresnationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac197ea5a010be96f89091140b72d7d856b1841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0c75905b3db493610efb3ca1d8d915aec54410")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.PresnationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.PresnationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
