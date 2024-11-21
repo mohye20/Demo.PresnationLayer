2ba33ab6e75becd9f02aa514cc6adc59a2a21a63
mohye <mohye20@gmail.com>
2024-11-21 11:04:47 +0200

    Send Email Function

--- Demo.PresnationLayer/Helpers/EmailSettings.cs
+++ Demo.PresnationLayer/Helpers/EmailSettings.cs
@@ -1,12 +1,18 @@
 ﻿using Demo.DataAcessLayer.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace Demo.PresnationLayer.Helpers
 {
     public static class EmailSettings
     {
-        public static void SendEmail(Email email) { 
-        
-        }
+        public static void SendEmail(Email email)
+        {
+            var Client = new SmtpClient("smtp.gmail.com",587);  
+            Client.EnableSsl = true;
+            Client.Credentials = new NetworkCredential("mohye20@gmail.com", "fkid tcgt birf ioks");
+            Client.Send("mohye20@gmail.com",email.To,email.Subject,email.Body);
 
+        }
     }
 }
